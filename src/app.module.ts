20ecd56ee03b574572738be0fea662d59cedea05
Ricardo Menotti <ricardomenotti31@gmail.com>
2022-05-29 13:25:51 -0500

    apollo sanbox added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { DonationsModule } from "./donations/donations.module";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ["./**/*.graphql"],
+    }),
+    DonationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
